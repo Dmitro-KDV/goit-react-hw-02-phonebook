7d494e8121275039040f9fd759717640b4843cd2
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-09-07 13:04:42 +0300

    com2

--- src/components/Phonebook/Contacts.jsx
+++ src/components/Phonebook/Contacts.jsx
@@ -1,4 +1,4 @@
-export const Contacts = ({contact}) => {
+export const Contacts = ({contact, handDelete}) => {
     return ( 
             <li>{contact.name}: {contact.number}
                 <button type='button' onClick={()=>handDelete(contact.contacts.id)}>Delete</button>
